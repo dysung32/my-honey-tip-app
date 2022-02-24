b7d5af8e259262b0fe6cc37e1b507ac8f6bd13ca
yoni <sdy32@naver.com>
2022-02-24 19:34:35 +0900

    카테고리 분류 및 디테일 페이지 완성

--- pages/DetailPage.js
+++ pages/DetailPage.js
@@ -1,16 +1,49 @@
-import React from 'react'
-import {View, Text, StyleSheet, Image, ScrollView, TouchableOpacity} from 'react-native'
+import React, { useEffect, useState } from 'react';
+import {View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, Alert, Share} from 'react-native';
+import * as Linking from 'expo-linking';
 
-export default function DetailPage() {
-  const tip = {
+export default function DetailPage({navigation, route}) {
+  const [tip, setTip] = useState({
     "idx": 9,
     "category": "재테크",
     "title": "렌탈 서비스 금액 비교해보기",
-    "image": "https://storage.googleapis.com/sparta-image.appspot.com/lecture/money1.png",
+    "image": "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Frental.png?alt=media&token=97a55844-f077-4aeb-8402-e0a27221570b",
     "desc": "요즘은 정수기, 공기 청정기, 자동차나 장난감 등 다양한 대여서비스가 활발합니다. 사는 것보다 경제적이라고 생각해 렌탈 서비스를 이용하는 분들이 늘어나고 있는데요. 다만, 이런 렌탈 서비스 이용이 하나둘 늘어나다 보면 그 금액은 겉잡을 수 없이 불어나게 됩니다. 특히, 렌탈 서비스는 빌려주는 물건의 관리비용까지 포함된 것이기에 생각만큼 저렴하지 않습니다. 직접 관리하며 사용할 수 있는 물건이 있는지 살펴보고, 렌탈 서비스 항목에서 제외해보세요. 렌탈 비용과 구매 비용, 관리 비용을 여러모로 비교해보고 고민해보는 것이 좋습니다. ",
     "date": "2020.09.09"
-}
+  });
+
+  useEffect(() => {
+    console.log(route)
+
+    // Card.js에서 navigation.navigate 함수를 쓸 때 두번째 인자로 content를 넘겨줬음
+    // content는 딕셔너리 그 자체였으므로 route.params에 고대로 남겨옵니다.
+    // 즉, route.params는 content죠!
+    navigation.setOptions({
+      // setOptions로 페이지 타이틀도 지정 가능하고
+      title: route.params.title,
+      //  작성했던 옵션을 다시 수정할 수도 있습니다.
+      headerStyle: {
+        backgroundColor: "#000",
+        shadowColor: "#000",
+      },
+      headerTintColor: "#fff"
+    });
+    setTip(route.params)
+  },[])
   
+  const popup = () => {
+    Alert.alert("팝업!!")
+  }
+
+  const share = () => {
+    Share.share({
+      message: `${tip.title} \n\n ${tip.desc} \n\n ${tip.image}` // 문자열
+    });
+  }
+
+  const link = () => {
+    Linking.openURL("https://mylifework.tistory.com/")
+  }
   
   return (
   <ScrollView style={styles.container}>
@@ -18,9 +51,18 @@ export default function DetailPage() {
     <View style={styles.textContainer}>
       <Text style={styles.title}>{tip.title}</Text>
       <Text style={styles.desc}>{tip.desc}</Text>
-      <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btnText}>팁 찜하기</Text>
-      </TouchableOpacity>
+      <View style={styles.btnGroup}>
+        <TouchableOpacity style={styles.btn} onPress={() => popup()}>
+          <Text style={styles.btnText}>팁 찜하기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => share()}>
+          <Text style={styles.btnText}>팁 공유하기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => link()}>
+          <Text style={styles.btnText}>외부 링크</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   </ScrollView>)
 }
@@ -50,11 +92,15 @@ const styles = StyleSheet.create({
     color: "#fff",
     marginTop: 20
   },
+  btnGroup: {
+    flexDirection: "row"
+  },
   btn: {
     borderColor: "deeppink",
-    borderWidth: 1,
+    backgroundColor: "deeppink",
     borderRadius: 7,
     marginTop: 30,
+    marginHorizontal: 10,
     width: 100,
     padding: 10
   },
--- pages/MainPage.js
+++ pages/MainPage.js
@@ -1,45 +1,97 @@
-import React from 'react';
-import main from '../assets/main.png';
+import React, { useEffect, useState } from 'react';
+import main from '../assets/main.jpg';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 import data from '../data.json';
+import Card from '../components/Card';
+import Loading from '../components/Loading';
+import { StatusBar } from 'expo-status-bar';
+import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
-export default function MainPage() {
-  let tip = data.tip;
-  let todayWeather = 10 + 17;
-  let todayCondition = "흐림";
+export default function MainPage({navigation, route}) {
   console.disableYellowBox = true;
-  return (
+
+  // useState 사용법
+  // [state, setState] 에서 state는 이 컴포넌트에서 관리될 상태 데이터를 담고 있는
+  // setState는 state를 변경시킬때 사용해야하는 함수
+
+  // 모두 다 useState가 선물해줌
+  // useState() 안에 전달되는 값은 state 초기값
+  // tip을 관리하기 위한 상태
+  const [state, setState] = useState([]);
+  // state = [] 빈 리스트(초기값)
+
+  // 카테고리에 따라 다른 꿀팁을 그때그때 저장 관리할 상태
+  const [cateState, setCateState] = useState([]);
+
+  // 현재 준비 상태인지, 준비를 끝낸 상태인지의
+  // 상태 데이터를 관리하기 위해 또 하나의 상태를 선언
+  const [ready, setReady] = useState(true);
+
+  // 하단의 return 문이 실행되어 화면이 그려진 다음 실행되는 useEffect 함수
+  // 내부에서 data.json으로부터 가져온 데이터를 state 상태에 담고 있음.
+  useEffect(()=>{
+
+    setTimeout(()=> { // 1000 ms 뒤에 함수 실행(지연 함수)
+      // 상태 관리에 들어간다
+      // 헤더의 타이틀 변경
+      navigation.setOptions({
+        title: "나만의 꿀팁"
+      })
+      // 꿀팁 데이터로 모두 초기화 준비
+      let tip = data.tip;
+      setState(tip) // 꿀팁 전체를 상태 관리 저장
+      setCateState(tip)
+      setReady(false) // 준비가 끝남 (상태가 변경되면 화면이 다시 그려짐)
+    }, 1500)
+
+  },[])
+
+  const category = (cate) => {
+    if(cate == "전체 보기") {
+      // "전체 보기"이면 전체 꿀팁 데이터를 담고 있는 상태값(state)로 다시 초기화
+      setCateState(state)
+    } else {
+      setCateState(state.filter((d) => {
+        // .filter의 역할: return 이 true인 애들만 골라서 리스트를 생성하여 넘겨줌
+        return d.category == cate
+      }))
+    }
+  }
+
+  // data.json 데이터는 state에 담기므로 상태에서 꺼내옴
+  let tip = state.tip;
+  let todayWeather = 10 + 17;
+  let todayCondition = "맑음";
+
+  // ready 상태가 true면 : 앞부분 실행 false면 : 뒷부분 실행
+  return ready ? <Loading /> : (
     <ScrollView style={styles.container}>
-      <Text style={styles.title}>나만의 꿀팁</Text>
+      <StatusBar style="black" />
       <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition}</Text>
       <Image style={styles.imageStyle} source={main} />
       <ScrollView style={styles.middleContainer} horizontal={true}>
-        <TouchableOpacity style={styles.middleButton1}>
-          <Text style={styles.middleButtonText}>생활</Text>
+        <TouchableOpacity style={styles.middleButtonAll} onPress={() => {category('전체 보기')}}>
+          <Text style={styles.middleButtonText} onPress={() => {category('전체 보기')}}>전체 보기</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton2}>
-          <Text style={styles.middleButtonText}>재테크</Text>
+        <TouchableOpacity style={styles.middleButton1} onPress={() => {category('생활')}}>
+          <Text style={styles.middleButtonText} onPress={() => {category('생활')}}>생활</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton3}>
-          <Text style={styles.middleButtonText}>반려견</Text>
+        <TouchableOpacity style={styles.middleButton2} onPress={() => {category('재테크')}}>
+          <Text style={styles.middleButtonText} onPress={() => {category('재테크')}}>재테크</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.middleButton4}>
-          <Text style={styles.middleButtonText}>꿀팁 찜</Text>
+        <TouchableOpacity style={styles.middleButton3} onPress={() => {category('반려견')}}>
+          <Text style={styles.middleButtonText} onPress={() => {category('반려견')}}>반려견</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton4} onPress={() => {category('꿀팁 찜')}}>
+          <Text style={styles.middleButtonText} onPress={() => {category('꿀팁 찜')}}>꿀팁 찜</Text>
         </TouchableOpacity>
       </ScrollView>
       <View style={styles.cardContainer}>
-      { 
-          tip.map((content,i)=>{ // 반복문
-            return (<View style={styles.card} key={i}>
-              <Image style={styles.cardImage} source={{uri:content.image}}/>
-              <View style={styles.cardText}>
-                <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
-                <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
-                <Text style={styles.cardDate}>{content.date}</Text>
-              </View>
-            </View>)
+        {/* 하나의 카드 영역을 나타내는 View */
+          cateState.map((content, i)=>{ // 반복문
+            return (<Card content={content} key={i} navigation={navigation} />)
           })
-         }
+        }
       </View>
     </ScrollView>
     
@@ -48,33 +100,38 @@ export default function MainPage() {
 
 const styles = StyleSheet.create({
   container: {
+    // 앱의 배경 색
     backgroundColor: "#fff"
   },
-  title: {
-    fontSize: 25,
-    fontWeight: "700",
-    color: "#212121",
-    marginTop: 40,
-    paddingLeft: 15,
-    paddingTop: 5,
-    paddingBottom: 5,
-    backgroundColor: "rgb(255,231,167)",
-    borderRadius: 30
-  },
   weather:{
-    alignSelf:"flex-end",
-    paddingRight:20
+    alignSelf: "flex-end",
+    paddingRight: 20,
+    marginBottom: 10
   },
   imageStyle: {
-    width: "100%",
-    height: 210,
+    width: "90%", // 컨텐츠의 넓이 값
+    height: 210, // 컨텐츠의 높이 값
+    // 컨텐츠 자체가 앱에서 어떤 곳에 위치시킬지 결정(정렬기능)
+    alignSelf: "center",
+    borderRadius: 20,
+    borderColor: "rgb(255,231,167)",
+    borderWidth: 3
   },
   middleContainer: {
     paddingLeft: 7,
     height: 60,
+    marginTop: 10
+  },
+  middleButtonAll: {
+    width: 90,
+    height: 47,
+    padding: 15,
+    backgroundColor: "#fdc453",
+    borderRadius: 15,
+    margin: 7
   },
   middleButton1: {
-    width: 100,
+    width: 90,
     height: 47,
     padding: 15,
     backgroundColor: "#fdc453",
@@ -82,7 +139,7 @@ const styles = StyleSheet.create({
     margin: 7
   },
   middleButton2: {
-    width: 100,
+    width: 90,
     height: 47,
     padding: 15,
     backgroundColor: "#fe8d6f",
@@ -90,7 +147,7 @@ const styles = StyleSheet.create({
     margin: 7
   },
   middleButton3: {
-    width: 100,
+    width: 90,
     height: 47,
     padding: 15,
     backgroundColor: "#9adbc5",
@@ -98,7 +155,7 @@ const styles = StyleSheet.create({
     margin: 7
   },
   middleButton4: {
-    width: 100,
+    width: 90,
     height: 47,
     padding: 15,
     backgroundColor: "#f886a8",
@@ -113,32 +170,5 @@ const styles = StyleSheet.create({
   cardContainer: {
     marginTop: 15,
     marginLeft: 7
-  },
-  card: {
-    flex: 1,
-    flexDirection: "row",
-    margin: 10
-  },
-  cardImage: {
-    flex: 1,
-    width: 100,
-    height: 100,
-    borderRadius: 10
-  },
-  cardText: {
-    flex: 2,
-    marginLeft: 10
-  },
-  cardTitle: {
-    fontSize: 20,
-    fontWeight: "700"
-  },
-  cardDesc: {
-    fontSize: 15,
-    marginTop: 5
-  },
-  cardDate: {
-    fontSize: 12,
-    marginTop: 7
   }
-})
+});
